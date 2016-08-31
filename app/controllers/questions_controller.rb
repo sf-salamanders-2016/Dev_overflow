8c4daeafb98aee86440fbabc41e8f2e030372b42
Blake Wills <blake.wills525@gmail.com>
2016-08-31 14:53:36 -0700

    Create boiler plate for new question and update questions controller

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,44 @@
+get '/questions' do
+  @questions = Question.order(:created_at)
+  erb :'questions/show'
+end
+
+get '/questions/new' do
+  erb :'questions/new'
+end
+
+post '/questions' do
+  @question = Question.new(params[:question])
+  if @question.save
+    redirect "/questions"
+    #we can also redirect to questions/id
+  else
+    @errors = @question.errors.full_messages
+    erb :'questions/new'
+  end
+end
+
+#If we decide to create individual question pages: 
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+  erb :'question/show' #or individual boiler plate
+end
+
+get '/questions/:id/edit' do
+  @user = User.find(params[:display_name])
+  @question = Question.find(params[:id])
+  erb :'question/edit' #or question/edit page boiler plate 
+end
+
+post '/questions/:id' do
+  @user = User.find(params[:display_name])
+  @question = Question.find(params[:id])
+  @question.update_attributes(params[:question])
+  redirect "/questions/#{@question.id}"
+end
+
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+  @question.destroy
+  redirect '/questions'
+end
