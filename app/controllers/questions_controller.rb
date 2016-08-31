12c197ea0b371e67be614d5cb45e995f719a6d41
Blake Wills <blake.wills525@gmail.com>
2016-08-31 12:22:53 -0700

    Create questions controller with all seven routes

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,42 @@
+get '/questions' do
+  @questions = Question.order(:created_at)
+  erb :'questions/show'
+end
+
+get '/questions/new' do
+  erb :'questions/new'
+end
+
+post '/questions' do
+  @question = Question.new(params[:question])
+  if @question.save
+    redirect "/questions/#{@question.id}"
+    #we can also redirect to questions 
+  else
+    @errors = @question.errors.full_messages
+    erb :'questions/new'
+  end
+end
+
+#If we decide to create individual question pages: 
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+  erb :'question/show' #or individual boiler plate
+end
+
+get '/questions/:id/edit' do
+  @question = Question.find(params[:id])
+  erb :'question/show' #or question/edit page boiler plate 
+end
+
+post '/questions/:id' do
+  @question = Question.find(params[:id])
+  @question.update_attributes(params[:question])
+  redirect "/questions/#{@question.id}"
+end
+
+get '/questions/:id' do
+  @question = Question.find(params[:id])
+  @question.destroy
+  redirect '/questions'
+end
