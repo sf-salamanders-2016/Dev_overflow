f77938770bfb00f494911bdcde779a18084cef8a
John Kim <joimkim@gmail.com>
2016-08-31 14:39:05 -0700

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   has_many :answers
   has_many :comments
   has_many :votes
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
 end
